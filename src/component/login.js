6bd8914d5cc8ce03808a4c6d1fd632f4da8d62ec
AishwaryaKapile <aishwaryakapile40@gmail.com>
2023-04-22 14:37:37 +0530

    error fixed

--- src/component/login.js
+++ src/component/login.js
@@ -20,7 +20,7 @@ const Login = () => {
         console.log(user);
       })
       .catch((error) => {
-        const errorCode = error.code;
+        //const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorMessage);
       });
